2600d672f87b3cbe2155e8d50de762e2c2692a3c
vivekdevkar123 <mrvivekdevkar123@gmail.com>
2025-12-09 20:07:35 +0530

    Replace all SVGs with placeholder images from picsum.photos

--- src/components/Layout/Header/Logo/index.tsx
+++ src/components/Layout/Header/Logo/index.tsx
@@ -7,7 +7,7 @@ const Logo: React.FC = () => {
   return (
     <Link href="/" className='flex items-center text-black dark:text-white text-2xl font-semibold gap-4'>
         <Image
-          src={imageUrl('/images/logo/logo.png')}
+          src={imageUrl('/images/logo/icon.png')}
         alt="logo"
         width={160}
         height={50}
