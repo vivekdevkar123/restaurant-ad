1c6ee378a5a2577437770ecca133cd39028fdb97
vivekdevkar123 <mrvivekdevkar123@gmail.com>
2025-12-13 18:29:42 +0530

    logo changed

--- src/components/Documentation/LogoConfiguration.tsx
+++ src/components/Documentation/LogoConfiguration.tsx
@@ -14,7 +14,7 @@ export const LogoConfiguration = () => {
           <div className="text-sm text-gray-400">
             <p>&#x3C;Link href=&#x22;/&#x22;&#x3E;</p>
             <p>&#x3C;Image</p>
-            <p>src=&#x22;/images/logo/logo.svg&#x22;</p>
+            <p>src=&#x22;/images/logo/logo.jpg&#x22;</p>
             <p>alt=&#x22;logo&#x22;</p>
             <p>width={160}</p>
             <p>height={50}</p>
--- src/components/Layout/Header/Logo/index.tsx
+++ src/components/Layout/Header/Logo/index.tsx
@@ -7,11 +7,11 @@ const Logo: React.FC = () => {
   return (
     <Link href="/" className='flex items-center text-black dark:text-white text-2xl font-semibold gap-4'>
         <Image
-          src={imageUrl('/images/logo/icon.png')}
+          src={imageUrl('/images/logo/icon.jpg')}
         alt="logo"
         width={160}
         height={50}
-        style={{ width: 'auto', height: 'auto' }}
+        className='h-12 w-auto object-contain max-w-[120px] rounded-lg'
         quality={100}
       />
   Sopu's Cloud kitchen
